e3ce8b851794cce9f90e76e067b1fc59c8d4d7e0
hozlucas28 <hozlucas28@gmail.com>
2022-09-08 16:08:50 -0300

    Add 'Cadena' lesson

---
+++ Clases/Cadena/libreria/libreria.c
@@ -0,0 +1,57 @@
+
+/* ------------------------------- Inclusiones ------------------------------ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "libreria.h"
+
+
+
+/* ------------------------- Funciones Desarrolladas ------------------------ */
+
+void crearSecuenciaPalabras (SecuenciaPalabras* secuencia, char* cadena) {
+    secuencia->cursor = cadena;
+    secuencia->finSecuencia = false;
+}
+
+
+bool leerPalabra (SecuenciaPalabras* secuencia, Palabra* palabra) {
+    char* cursor = secuencia->cursor;
+    while (*cursor && !esLetra(*cursor)) {
+        cursor++;
+    };
+
+    if (!*cursor) {
+        secuencia->finSecuencia = true;
+        secuencia->cursor = cursor;
+        return false;
+    };
+
+    palabra->inicio = cursor;
+
+    while (*cursor && esLetra(*cursor)) {
+        cursor++;
+    };
+
+    palabra->fin = cursor - 1;
+    secuencia->cursor = cursor;
+    return true;
+}
+
+
+bool finSecuencia (const SecuenciaPalabras* secuencia) {
+    return secuencia->finSecuencia;
+}
+
+
+void mostrarPalabra (const Palabra* palabra) {
+    for (char* c = palabra->inicio; c <= palabra->fin; c++) {
+        putchar(*c);
+    };
+}
+
+
+bool esLetra (char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
